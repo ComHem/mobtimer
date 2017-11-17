55e159e18c81b78512dece644250a2df3f6078dd
Edvin <edvin.berg@comhem.com>
2017-11-17 11:38:57 +0100

    minor deployscript fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,7 +14,7 @@ case "${BRANCH}" in
     ;;
 esac
 
-repo="${registry}/mobtimer”
+repo="${registry}/mobtimer"
 image=${repo}:${VERSION}
 
 echo "Creating and publishing docker image..." && \
@@ -22,8 +22,7 @@ docker login -u=${DOCKER_USERNAME} -p=${DOCKER_PASSWORD} ${DOCKER_REGISTRY} && \
 docker build -t ${image} . && \
 docker tag ${image} ${repo}:${tag} && \
 docker push ${image} && \
-echo "Deploying mobtimer to cluster…” 
-# && \
-#kubectl apply -f kubernetes/quantum-configmap.yml --record && \
-#kubectl set image deployment/quantum-deployment quantum=${image} --record && \
-#kubectl rollout status deployment/quantum-deployment && \
+echo "Deploying mobtimer to cluster…" && \
+kubectl apply -f kubernetes/mobtimer-configmap.yml --record && \
+kubectl set image deployment/mobtimer-deployment mobtimer=${image} --record && \
+kubectl rollout status deployment/mobtimer-deployment && \

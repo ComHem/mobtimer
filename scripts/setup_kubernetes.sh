c34afa57e7f3a3bcc0f5773c9e23aa21011a2ba9
Edvin <edvin.berg@comhem.com>
2018-01-05 08:59:44 +0100

    temp change kubectl ver

--- scripts/setup_kubernetes.sh
+++ scripts/setup_kubernetes.sh
@@ -13,7 +13,7 @@ esac
 
 ### Install kubernetes
 echo "Downloading kubernetes binary..."
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.7.10/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 

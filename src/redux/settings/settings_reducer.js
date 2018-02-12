b33d1db215aa0f46cb1e2ee86c18edd2ccda4712
Pär Strandberg <par.strandberg@comhem.com>
2018-02-12 19:27:48 +0100

    Fixed bug when intermezzo probably started right away. breaking was set to 5 instead of boolean. Paused blur only animates on x-axis. Due to taking over and making it hard to use the next button clickable area.

--- src/redux/settings/settings_reducer.js
+++ src/redux/settings/settings_reducer.js
@@ -3,7 +3,7 @@ import * as types from './settings_types';
 const initial_state = {
     sessionLength: 10,
     breakInterval: 4,
-    breaking: 10,
+    breaking: false,
     strings: {
         sessionLength: 'Length of each turn (min)',
         breakInterval: 'Break every (n) round',
